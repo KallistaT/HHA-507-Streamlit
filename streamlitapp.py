ea0414d9640463085281f2e7d564f61e1a0356ca
Kallista Tong <kalt9616@gmail.com>
2022-03-11 13:47:42 -0500

    edited file

--- streamlitapp.py
+++ streamlitapp.py
@@ -37,8 +37,8 @@ st.title('HHA 507 Final Assignment: E2E with Streamlit')
 st.subheader('Kallista Tong')
 st.write('Questions:')
 st.write('1. How does Stony Brook Hospital''s rating compare with the rest of NY?')
-st.write('2. What is the most expensive inpatient DRGs for Stony Brook?')
-st.write('3. What is the most expensive outpatient APCs for Stony Brook?')
+st.write('2. What is the most expensive inpatient DRGs for Stony Brook University Hospital?')
+st.write('3. What is the most expensive outpatient APCs for Stony Brook University Hospital?')
 st.write('4. ...')
 st.write('5. ...')
 st.write('6. ...')    
@@ -72,5 +72,24 @@ sbu_df = hospital_df[hospital_df['hospital_name'] == 'SUNY/STONY BROOK UNIVERSIT
 st.header('Stony Brook University Hospital')
 st. markdown('This dataset displays information on Stony Brook University Hospital')
 st.dataframe(sbu_df)
+
+# Question 1
+rating_table = ny_df['hospital_overall_rating'].value_counts().reset_index()
+st.header('How does Stony Brook Hospital''s overall rating compare to the rest of New York State hospitals?')
+st.subheader('New York State Hospital Ratings')
+st.markdown('The following table shows the distribution of hospital ratings across New York State. From the table above, we can see that Stony Brook University Hospital has an overall rating of 4, landing in the top 10-11% highest hospital ratings in New York.')
+st.dataframe(rating_table)
+
+#Question 2
+inpatient_table = inpatient_df[inpatient_df['provider_id'] == 330393]
+st.header('Stony Brook University Hospital inpatient data')
+st.markdown('The following table shows inpatient data for Stony Brook University hospital.')
+st.dataframe(inpatient_table)
+
+st.header('What is the most expensive inpatient DRGs code for Stony Brook University Hospital?')
+st.subheader('Stony Brook Hospital Inpatient DRGs')
+inpatient_df_DRGs = inpatient_df[inpatient_df['average_total_payments']]
+st.bar_chart(inpatient_df_DRGs)
+
 
 
